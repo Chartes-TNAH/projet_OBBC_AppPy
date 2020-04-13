fb43e23b9da7735f1dde1ea035f3a74a05335c41
Lucas Terriel <ls.terriel@gmail.com>
2020-04-13 12:44:02 +0200

    corr 500 error

--- OBBC/app/routes.py
+++ OBBC/app/routes.py
@@ -269,5 +269,5 @@ def page_not_found(error):
     return render_template('errors/404.html'), 404
 
 @app.errorhandler(500)
-def page_not_found(error):
+def server_error(error):
     return render_template('errors/500.html'), 500
